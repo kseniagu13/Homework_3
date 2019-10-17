cdcfc35ddc8bac74cb169d15448fb17f639eab39
Ksenia Gustsenko <kseniagustsenko@Ksenias-MacBook-Air.local>
2019-10-17 20:17:51 +0300

    C or F

---
+++ Homework.Celsius/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework.Celsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start:
+            string userChoice;
+            double userAnswer;
+            double fahrenheit;
+            double celsius;
+
+            Console.WriteLine("Choose Celsius or Fahrenheit C/F: ");
+            userChoice = Console.ReadLine();
+
+            if (userChoice.ToUpper() == "F")
+            {
+                Console.WriteLine("Write a degree in Fahrenheit: ");
+                userAnswer = Double.Parse(Console.ReadLine());
+                celsius = (userAnswer - 32) * 5 / 9;
+                Console.WriteLine($"{userAnswer} is converted to celsius: {celsius} C.");
+            }
+
+            else if (userChoice.ToUpper() == "C")
+            {
+                Console.WriteLine("Write a degree in Celsius: ");
+                userAnswer = Double.Parse(Console.ReadLine());
+                fahrenheit = (userAnswer * 9 / 5) + 32;
+                Console.WriteLine($"{userAnswer} is converted to fahrenheit: {fahrenheit} F.");
+            }
+            else
+            {
+                Console.WriteLine("Choose F (fahrenheit) or C (celsius)");
+              
+            }
+            goto Start;
+            
+            Console.ReadLine();
+
+        }
+    }
+}
